7863d09c4edb9ff839e999c7ad87bb0356ffcca4
mr-geones <105590080+mr-geones@users.noreply.github.com>
2023-02-26 19:34:15 +0100

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,19 @@
+function fizzBuzz() {
+	let arr = [];
+	
+	for(let i = 1; i <= 100; i++) {
+		if(i % 3 === 0 && i % 5 === 0) {
+			arr.push('FizzBuzz');
+		}
+		else if(i % 5 === 0) {
+			arr.push('Buzz');
+		}
+		else if(i % 3 === 0) {
+			arr.push('Fizz');
+		}
+		else {
+			arr.push(i)
+		}
+	}
+	return arr;
+}
